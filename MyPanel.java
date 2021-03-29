ddbca51ad27f8a66aceb1222454f72c5717682c5
gao-lidijia <56687255+gao-lidijia@users.noreply.github.com>
2021-03-29 00:00:06 -0400

    Update MyPanel.java

--- MyPanel.java
+++ MyPanel.java
@@ -1,22 +1,66 @@
 import java.awt.Color;
 import java.awt.Graphics;
-
+import java.util.ArrayList;
+import java.awt.event.*;
 import javax.swing.JPanel;
 
-public class MyPanel extends JPanel{
+public class MyPanel extends JPanel implements ActionListener, MouseListener, MouseMotionListener{
 	
 	SingleBlock bob = new SingleBlock();
+	ArrayList<Pieces> piece = new ArrayList<Pieces>();
+	int xpos1 = 75;
+	int xpos2 = 175;
+	int xpos3 = 275;
+	int ypos = 350;
+
 	
 	public MyPanel() {
+		for(int i = 0; i<3; i++)
+			piece.add(new Pieces());
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		this.setBackground(new Color(201,233,246));
-        
-		g.drawRect(300, 300, 30, 30);
-		
 		bob.myDraw(g);
 		
+		piece.get(0).myDraw(g, xpos1, ypos);
+		
+		
+		
+		piece.get(1).myDraw(g, xpos2, ypos);
+		piece.get(2).myDraw(g, xpos3, ypos);
+		
+			
+	
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		
 	}
+	public void actionPerformed(ActionEvent e) {}
+
+
+	public void mouseClicked(MouseEvent e) {}
+
+	
+	public void mousePressed(MouseEvent e) {}
+
+
+	public void mouseReleased(MouseEvent e) {}
+
+
+	public void mouseEntered(MouseEvent e) {}
+
+	
+	public void mouseExited(MouseEvent e) {}
+	
+	public void mouseDragged(MouseEvent e) {
+		int xMouse = e.getX();
+		int yMouse = e.getY();
+		repaint();
+		
+	}
+	public void mouseMoved(MouseEvent e) {}
+
+	
 }
